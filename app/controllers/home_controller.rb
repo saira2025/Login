f043cf5c69bb89411c7dbe5397b0cb2a8618e6b8
saira2025 <sairamunawar2025@gmail.com>
2022-09-01 13:32:31 +0500

    Pundit gem install

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,4 @@
 class HomeController < ApplicationController
   def index
-    def index
-      if current_admin_user
-       @email = current_admin_user.email
-      else
-       redirect_to new_admin_user_session_path, notice: 'You have successfully logged out!'
-      end
-    end
   end
 end
